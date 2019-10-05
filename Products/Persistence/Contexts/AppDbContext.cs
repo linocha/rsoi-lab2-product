6c9f980bc761047c3c721e0ee2760eed92253f0a
Angelina Y <linocha1320@gmail.com>
2019-10-05 19:43:49 +0300

    update

--- Products/Persistence/Contexts/AppDbContext.cs
+++ Products/Persistence/Contexts/AppDbContext.cs
@@ -5,9 +5,19 @@ namespace Products.Persistence.Contexts
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        public DbSet<Product> Products { get; set; }
+        
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().ToTable("Products", schema: "products-schema");
+            modelBuilder.Entity<Product>().HasKey(p => p.Id);
+            modelBuilder.Entity<Product>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+            modelBuilder.Entity<Product>().Property(p => p.Name).IsRequired().HasMaxLength(30);
+            modelBuilder.Entity<Product>().Property(p => p.Cost).IsRequired();
         }
     }
 }
